2fa35f90edc4ccaaf2cbd4338efeea5f97553aaa
Syed Kumail Raza <136350543+syedkumailraza1@users.noreply.github.com>
2024-07-02 16:44:09 +0530

    Code pushed

---
+++ OfficeProject/OfficeProject/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace OfficeProject.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string city { get; set; }
+        public string skills { get; set; }
+    }
+}
---
+++ OfficeProject/OfficeProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OfficeProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
