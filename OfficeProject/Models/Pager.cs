629965c98f668e33f059f4381657bd2f1c677f30
Syed Kumail Raza <136350543+syedkumailraza1@users.noreply.github.com>
2024-07-08 16:31:59 +0530

    pagination added

---
+++ OfficeProject/Models/Pager.cs
@@ -0,0 +1,45 @@
+﻿namespace OfficeProject.Models
+{
+    public class Pager
+    {
+        public int TotalItems { get; private set; }
+        public int TotalPages { get; private set; }
+        public int CurrentPage { get; private set; }
+        public int PageSize { get; private set; }
+        public int StartPage { get; private set; }
+        public int EndPage { get; private set; }
+
+        public Pager()
+        {
+            // Default constructor
+        }
+
+        public Pager(int totalItems, int page, int pageSize = 10)
+        {
+            TotalItems = totalItems;
+            PageSize = pageSize;
+
+            // Calculate total pages after assigning PageSize
+            TotalPages = (int)Math.Ceiling((decimal)TotalItems / PageSize);
+            CurrentPage = page;
+
+            StartPage = CurrentPage - 5;
+            EndPage = CurrentPage + 4;
+
+            if (StartPage <= 0)
+            {
+                EndPage -= (StartPage - 1);
+                StartPage = 1;
+            }
+
+            if (EndPage > TotalPages)
+            {
+                EndPage = TotalPages;
+                if (EndPage > 10)
+                {
+                    StartPage = EndPage - 9;
+                }
+            }
+        }
+    }
+}
